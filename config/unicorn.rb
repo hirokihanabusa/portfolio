1c26d9743a6416ef213c8754f5da244d448e490c
hirokihanabusa <clouscolsunset@yahoo.co.jp>
2020-01-25 19:00:48 +0900

    initial commit

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@
 app_path = File.expand_path('../../', __FILE__)
 
 #アプリケーションサーバの性能を決定する
-worker_processes 3
+worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
 working_directory app_path
@@ -20,7 +20,7 @@ stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
-timeout 300
+timeout 60
 
 #以下は応用的な設定なので説明は割愛
 
